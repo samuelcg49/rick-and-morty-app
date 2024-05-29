4ddb862ea2ce52442f34859e078773c419c80121
samuelcg49 <samuelcg49@gmail.com>
2024-05-29 21:35:35 +0200

    Pantalla detalle de episodios

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+
 function LocationCard(props: any) {
-  const { name, type, dimension } = props.location;
+  const { id, name, type, dimension } = props.location;
 
   return (
     <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow sm:flex-row sm:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
       <div className="flex flex-col justify-between p-4 pl-8 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <Link
+          to={`/locations/${id}`}
+          className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-[#40b5cb]"
+        >
           {name}
-        </h5>
+        </Link>
 
         <p className="font-bold text-gray-700 dark:text-gray-400">Type</p>
         <p>{type}</p>
--- src/pages/LocationDetail.tsx
+++ src/pages/LocationDetail.tsx
@@ -1,5 +1,52 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
+import CharacterCard from "../components/CharacterCard";
+
 function LocationDetail() {
-  return <div>LocationDetail</div>;
+  const { id } = useParams<{ id: string }>();
+  const [location, setLocation] = useState<any>({});
+  const [characters, setCharacters] = useState<any[]>([]);
+
+  const fetchLocation = async () => {
+    try {
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/location/${id}`
+      );
+      const locationData = response.data;
+      setLocation(locationData);
+      console.log(locationData);
+      const charactersPromises = locationData.residents.map(
+        (characterURL: string) => axios.get(characterURL)
+      );
+      const charactersResponses = await Promise.all(charactersPromises);
+      const charactersData = charactersResponses.map((res) => res.data);
+
+      setCharacters(charactersData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchLocation();
+  }, [id]);
+
+  return (
+    <>
+      <div className="mb-12 flex flex-col items-center">
+        <h1 className="mb-5 text-4xl font-bold">{location.name}</h1>
+        <p>
+          {location.type} - {location.dimension}
+        </p>
+        <h2 className="mt-12 mb-7 text-xl font-bold">Residents</h2>
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-7">
+          {characters.map((item: any) => (
+            <CharacterCard key={item.id} character={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LocationDetail;
