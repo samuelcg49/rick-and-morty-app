ef0b1d2e90cb4f121a911e3f7673e115ecca13a5
samuelcg49 <samuelcg49@gmail.com>
2024-05-29 21:58:52 +0200

    fixed import bug

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -5,14 +5,14 @@ function Pagination(props: any) {
         <button
           className="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           onClick={props.paginaAnterior}
-          >
+        >
           Previous
         </button>
 
         <button
           className="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           onClick={props.paginaSiguiente}
-          >
+        >
           Next
         </button>
       </div>
